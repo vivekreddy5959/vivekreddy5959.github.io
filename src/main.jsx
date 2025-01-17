670856c1f852c9a546c6cc9bf2a5a9554f35d029
vivekreddy5959 <150700165+vivekreddy5959@users.noreply.github.com>
2025-01-17 15:18:02 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import ReactGA from "react-ga4";
 
-ReactGA.initialize("G-61C0P4SLGG");
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
